fca1afe98c477404a4318c5c788b0e4de88c5ba3
Shawn Roller <6394755+ShawnRoller@users.noreply.github.com>
2024-11-24 10:34:56 -0500

    Initial Commit

---
+++ LiveActivityOfflineDemo/LiveActivityOfflineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityOfflineDemoApp.swift
+//  LiveActivityOfflineDemo
+//
+//  Created by Invitation Homes on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityOfflineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
